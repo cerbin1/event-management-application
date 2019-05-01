1b11dc71f3933896f702681818165aa9008a7458
bartek <gekarez@gmail.com>
2019-05-01 17:59:46 +0200

    Actualize form create event form fields and Event model

--- src/main/java/pl/wydarzenia/model/Event.java
+++ src/main/java/pl/wydarzenia/model/Event.java
@@ -2,14 +2,17 @@ package pl.wydarzenia.model;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 public class Event {
     private String name;
+    private String category;
+    private String place;
     private String organizerName;
-    private LocalDateTime dateOfTheEvent;
+    private LocalDate dateOfTheEvent;
     private String description;
     private int plannedNumberOfParticipants;
+    private String comments;
     private Organizer organizer;
 }
