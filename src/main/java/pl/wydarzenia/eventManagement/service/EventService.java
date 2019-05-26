60eaf7965f7d2ed90d0a29aef560ce68208142c9
bartek <gekarez@gmail.com>
2019-05-26 14:15:25 +0200

    Add getAcceptedEvents() and hardcoded test data to EventService

--- src/main/java/pl/wydarzenia/eventManagement/service/EventService.java
+++ src/main/java/pl/wydarzenia/eventManagement/service/EventService.java
@@ -1,11 +1,14 @@
 package pl.wydarzenia.eventManagement.service;
 
+import pl.wydarzenia.eventManagement.model.Event;
 import pl.wydarzenia.eventManagement.model.EventCategory;
 import pl.wydarzenia.eventManagement.model.EventPlace;
 
 import java.util.List;
 
 public interface EventService {
+    List<Event> getAcceptedEvents();
+
     List<EventCategory> getCategories();
 
     List<EventPlace> getPlaces();
--- src/main/java/pl/wydarzenia/eventManagement/service/EventServiceImpl.java
+++ src/main/java/pl/wydarzenia/eventManagement/service/EventServiceImpl.java
@@ -1,14 +1,94 @@
 package pl.wydarzenia.eventManagement.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.wydarzenia.eventManagement.model.Event;
 import pl.wydarzenia.eventManagement.model.EventCategory;
 import pl.wydarzenia.eventManagement.model.EventPlace;
+import pl.wydarzenia.eventManagement.model.EventStatus;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EventServiceImpl implements EventService {
+    private List<Event> events = Arrays.asList(
+            new Event("Nazwa 1",
+                    EventStatus.NEW,
+                    "01",
+                    "01",
+                    "Organizator 1",
+                    new Date(1000),
+                    "Opis 1",
+                    15,
+                    "komentarz 1",
+                    "Jan",
+                    "Kowalski",
+                    "123456789",
+                    "jan@kowalski.pl",
+                    true,
+                    false,
+                    false),
+            new Event("Nazwa 2",
+                    EventStatus.NEW,
+                    "01",
+                    "01",
+                    "Organizator 1",
+                    new Date(1000),
+                    "Opis 1",
+                    15,
+                    "komentarz 1",
+                    "Jan",
+                    "Kowalski",
+                    "123456789",
+                    "jan@kowalski.pl",
+                    true,
+                    false,
+                    false),
+            new Event("Nazwa 3",
+                    EventStatus.ACCEPTED,
+                    "01",
+                    "01",
+                    "Organizator 1",
+                    new Date(1000),
+                    "Opis 1",
+                    15,
+                    "komentarz 1",
+                    "Jan",
+                    "Kowalski",
+                    "123456789",
+                    "jan@kowalski.pl",
+                    true,
+                    false,
+                    false),
+            new Event("Nazwa 4",
+                    EventStatus.ACCEPTED,
+                    "01",
+                    "01",
+                    "Organizator 1",
+                    new Date(1000),
+                    "Opis 1",
+                    15,
+                    "komentarz 1",
+                    "Jan",
+                    "Kowalski",
+                    "123456789",
+                    "jan@kowalski.pl",
+                    true,
+                    false,
+                    false));
+
+    @Autowired
+    public List<Event> getAcceptedEvents() {
+        return events
+                .stream()
+                .filter(event -> event.getStatus().equals(EventStatus.ACCEPTED))
+                .collect(Collectors.toList());
+    }
+
+    @Autowired
     public List<EventCategory> getCategories() {
         return Arrays.asList(
                 new EventCategory("01", "Uroczystość"),
@@ -18,6 +98,7 @@ public class EventServiceImpl implements EventService {
                 new EventCategory("05", "Sportowe"));
     }
 
+    @Autowired
     public List<EventPlace> getPlaces() {
         return Arrays.asList(
                 new EventPlace("01", "Sala wykładowa"),
