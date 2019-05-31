511b5ac735c4f3750abcfae72c357405c541f9b3
bartek <gekarez@gmail.com>
2019-05-31 22:06:52 +0200

    Implement editing event in event management controller

--- src/main/java/pl/wydarzenia/eventManagement/controller/EventManagementController.java
+++ src/main/java/pl/wydarzenia/eventManagement/controller/EventManagementController.java
@@ -4,7 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.wydarzenia.eventManagement.model.Event;
+import pl.wydarzenia.eventManagement.model.EventCategory;
+import pl.wydarzenia.eventManagement.model.EventPlace;
 import pl.wydarzenia.eventManagement.service.EventService;
+
+import java.util.List;
 
 @Controller
 public class EventManagementController {
@@ -17,7 +25,32 @@ public class EventManagementController {
 
     @GetMapping("wydarzenia/zarzadzanie")
     public String getEventManagementPage(Model model) {
-        model.addAttribute("newEvents", eventService.getAllEvents());
+        model.addAttribute("events", eventService.getAllEvents());
         return "events_management";
+    }
+
+    @GetMapping("/zarzadzanieWydarzeniami/wydarzenie")
+    public String getEventManagementView(
+            Model model,
+            @RequestParam long eventId) {
+        Event event = eventService.getEventById(eventId);
+        model.addAttribute("event", event);
+        return "events_management_event";
+    }
+
+    @PostMapping("/zarzadzanieWydarzeniami/wydarzenie/zapisz")
+    public String saveEventEdit(Event event) {
+        // TODO: save
+        return "redirect:/wydarzenia/zarzadzanie";
+    }
+
+    @ModelAttribute("eventCategories")
+    public List<EventCategory> getEventCategories() {
+        return eventService.getCategories();
+    }
+
+    @ModelAttribute("eventPlaces")
+    public List<EventPlace> getEventPlaces() {
+        return eventService.getPlaces();
     }
 }
