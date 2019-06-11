76a1fe4c434cf499da95aa47140680893a446708
bartek <gekarez@gmail.com>
2019-06-11 15:38:33 +0200

    Remove unused class MvcConfig

--- src/main/java/pl/wydarzenia/config/MvcConfig.java
+++
@@ -1,8 +0,0 @@
-package pl.wydarzenia.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
-}
