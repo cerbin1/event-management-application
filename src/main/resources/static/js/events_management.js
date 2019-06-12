04537408f1dce0c228cc13efc9d24ea715f15b9a
bartek <gekarez@gmail.com>
2019-06-12 23:38:06 +0200

    Add nav tabs to events and personal data tables in events_management view

--- src/main/resources/static/js/events_management.js
+++ src/main/resources/static/js/events_management.js
@@ -1,8 +1,25 @@
-;$(document).ready(function () {
+$(document).ready(function () {
     initializeDataTables();
+    initializeButtonOnClickListeners();
 
     function initializeDataTables() {
         $('#newEventsTable').DataTable();
         $('#personDetailsTable').DataTable();
+    }
+
+    function initializeButtonOnClickListeners() {
+        $('#eventsLink').on('click', function () {
+            $('#eventsTable').css("display", "block");
+            $('#personalDataTable').css("display", "none");
+            $('#eventsLink').addClass('active');
+            $('#personalDataLink').removeClass('active');
+        });
+
+        $('#personalDataLink').on('click', function () {
+            $('#eventsTable').css("display", "none");
+            $('#personalDataTable').css("display", "block");
+            $('#personalDataLink').addClass('active');
+            $('#eventsLink').removeClass('active');
+        });
     }
 });
